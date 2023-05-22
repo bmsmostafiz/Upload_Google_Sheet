c734283d3e2c9fc43d74061825314921e6cabba1
bmsmostafiz <bms.mostafiz@gmail.com>
2023-05-22 13:02:56 +0600

    all

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbytX8NVzIsDjAjVNRxSPohKMUdV5M_L1uyF-mMjCuM9Kz9Il3pv9O-EUTCAjZhbdBd1/exec'
-        const form = document.forms['google-sheet']
+const form = document.forms['google-sheet']
+const msg = document.getElementById("msg");
 
-        form.addEventListener('submit', e => {
-            e.preventDefault()
-            fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-                .catch(error => console.error('Error!', error.message))
-                form.reset()
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
         })
+        .then(response => {
+            msg.innerHTML = "Thanks for Contacting us..!"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 3000)
+            form.reset()
+        })
+
+        .catch(error => console.error('Error!', error.message))
+})
